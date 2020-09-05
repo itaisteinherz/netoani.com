bf29ca0b46c02b35f4b5f4cf29a8e37a37b5f416
Itai Steinherz <itaisteinherz@gmail.com>
2020-09-05 17:39:00 +0300

    Fix indentation

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,24 +7,24 @@ exports.createPages = ({graphql, actions}) => {
 	const blogPost = path.resolve("./src/templates/blog-post.js");
 	return graphql(
 		`
-            {
-                allMarkdownRemark(
-                    sort: { fields: [frontmatter___date], order: DESC }
-                    limit: 1000
-                ) {
-                    edges {
-                        node {
-                            fields {
-                                slug
-                            }
-                            frontmatter {
-                                title
-                            }
-                        }
-                    }
-                }
-            }
-        `
+			{
+				allMarkdownRemark(
+					sort: { fields: [frontmatter___date], order: DESC }
+					limit: 1000
+				) {
+					edges {
+						node {
+							fields {
+								slug
+							}
+							frontmatter {
+								title
+							}
+						}
+					}
+				}
+			}
+		`
 	).then(result => { // eslint-disable-line promise/prefer-await-to-then
 		if (result.errors) {
 			throw result.errors;
@@ -34,8 +34,7 @@ exports.createPages = ({graphql, actions}) => {
 		const posts = result.data.allMarkdownRemark.edges;
 
 		posts.forEach((post, index) => {
-			const previous =
-                index === posts.length - 1 ? null : posts[index + 1].node;
+			const previous = index === posts.length - 1 ? null : posts[index + 1].node;
 			const next = index === 0 ? null : posts[index - 1].node;
 
 			createPage({
